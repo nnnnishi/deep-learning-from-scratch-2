9530b10353c4eb206f177950b8ad0faba387eb11
Naoki Nishimura <nishimura.n.ab@gmail.com>
2018-11-24 10:25:17 +0900

    ch01

---
+++ work/ch01/ch01.py
@@ -0,0 +1,296 @@
+#%% [markdown]
+## Chapter 1 ニューラルネットワークの復習
+
+#%% 
+'''
+1. 数学とPythonの復習
+1. ニューラルネットワークの推論
+1. ニューラルネットワークの学習
+1. ニューラルネットワークで問題を解く
+1. 計算の高速化
+1. まとめ
+'''
+
+#%% [markdown]
+## 2レイヤネットの実装
+
+#%%
+import sys
+sys.path.append('..')  # 親ディレクトリのファイルをインポートするための設定
+import numpy as np
+from common.layers import Affine, Sigmoid, SoftmaxWithLoss
+
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size):
+        I, H, O = input_size, hidden_size, output_size
+
+        # 重みとバイアスの初期化
+        W1 = 0.01 * np.random.randn(I, H)
+        b1 = np.zeros(H)
+        W2 = 0.01 * np.random.randn(H, O)
+        b2 = np.zeros(O)
+
+        # レイヤの生成
+        self.layers = [
+            Affine(W1, b1),
+            Sigmoid(),
+            Affine(W2, b2)
+        ]
+        self.loss_layer = SoftmaxWithLoss()
+
+        # すべての重みと勾配をリストにまとめる
+        self.params, self.grads = [], []
+        for layer in self.layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+    def predict(self, x):
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
+
+    def forward(self, x, t):
+        score = self.predict(x)
+        loss = self.loss_layer.forward(score, t)
+        return loss
+
+    def backward(self, dout=1):
+        dout = self.loss_layer.backward(dout)
+        for layer in reversed(self.layers):
+            dout = layer.backward(dout)
+        return dout
+#%%
+x = np.random.randn(10, 2)
+model = TwoLayerNet(2, 4, 3)
+s = model.predict(x)
+print(s)
+
+#%% [markdown]
+## MatMulレイヤの実装
+
+#%%
+class MatMul:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.x = None
+
+    def forward(self, x):
+        W, = self.params
+        out = np.dot(x, W)
+        self.x = x
+        return out
+
+    def backward(self, dout):
+        W, = self.params
+        dx = np.dot(dout, W.T)
+        dW = np.dot(self.x.T, dout)
+        self.grads[0][...] = dW
+        return dx
+
+#%% [markdown]
+## Affineレイヤの実装
+
+class Affine:
+    def __init__(self, W, b):
+        self.params = [W, b]
+        self.grads = [np.zeros_like(W), np.zeros_like(b)]
+        self.x = None
+
+    def forward(self, x):
+        W, b = self.params
+        out = np.dot(x, W) + b
+        self.x = x
+        return out
+
+    def backward(self, dout):
+        W, b = self.params
+        dx = np.dot(dout, W.T)
+        dW = np.dot(self.x.T, dout)
+        db = np.sum(dout, axis=0)
+
+        self.grads[0][...] = dW
+        self.grads[1][...] = db
+        return dx
+
+#%% [markdown]
+## Softmax with lossレイヤの実装
+
+#%%
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.y = None  # softmaxの出力
+        self.t = None  # 教師ラベル
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+
+        # 教師ラベルがone-hotベクトルの場合、正解のインデックスに変換
+        if self.t.size == self.y.size:
+            self.t = self.t.argmax(axis=1)
+
+        loss = cross_entropy_error(self.y, self.t)
+        return loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = self.y.copy()
+        dx[np.arange(batch_size), self.t] -= 1
+        dx *= dout
+        dx = dx / batch_size
+
+        return dx
+
+#%% [markdown]
+## spiralデータセット
+
+#%%
+import sys
+sys.path.append('..')  # 親ディレクトリのファイルをインポートするための設定
+from dataset import spiral
+import matplotlib.pyplot as plt
+
+
+x, t = spiral.load_data()
+print('x', x.shape)  # (300, 2)
+print('t', t.shape)  # (300, 3)
+
+# データ点のプロット
+N = 100
+CLS_NUM = 3
+markers = ['o', 'x', '^']
+for i in range(CLS_NUM):
+    plt.scatter(x[i*N:(i+1)*N, 0], x[i*N:(i+1)*N, 1], s=40, marker=markers[i])
+plt.show()
+
+#%% [markdown]
+## 2層ニューラルネットワーク
+
+#%%
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size):
+        I, H, O = input_size, hidden_size, output_size
+
+        # 重みとバイアスの初期化
+        W1 = 0.01 * np.random.randn(I, H)
+        b1 = np.zeros(H)
+        W2 = 0.01 * np.random.randn(H, O)
+        b2 = np.zeros(O)
+
+        # レイヤの生成
+        self.layers = [
+            Affine(W1, b1),
+            Sigmoid(),
+            Affine(W2, b2)
+        ]
+        self.loss_layer = SoftmaxWithLoss()
+
+        # すべての重みと勾配をリストにまとめる
+        self.params, self.grads = [], []
+        for layer in self.layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+    def predict(self, x):
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
+
+    def forward(self, x, t):
+        score = self.predict(x)
+        loss = self.loss_layer.forward(score, t)
+        return loss
+
+    def backward(self, dout=1):
+        dout = self.loss_layer.backward(dout)
+        for layer in reversed(self.layers):
+            dout = layer.backward(dout)
+        return dout
+
+#%%
+# coding: utf-8
+import sys
+sys.path.append('..')  # 親ディレクトリのファイルをインポートするための設定
+import numpy as np
+from common.optimizer import SGD
+from common.functions import softmax, cross_entropy_error
+from dataset import spiral
+import matplotlib.pyplot as plt
+
+
+# ハイパーパラメータの設定
+max_epoch = 300
+batch_size = 30
+hidden_size = 10
+learning_rate = 1.0
+
+x, t = spiral.load_data()
+model = TwoLayerNet(input_size=2, hidden_size=hidden_size, output_size=3)
+optimizer = SGD(lr=learning_rate)
+
+# 学習で使用する変数
+data_size = len(x)
+max_iters = data_size // batch_size
+total_loss = 0
+loss_count = 0
+loss_list = []
+
+for epoch in range(max_epoch):
+    # データのシャッフル
+    idx = np.random.permutation(data_size)
+    x = x[idx]
+    t = t[idx]
+
+    for iters in range(max_iters):
+        batch_x = x[iters*batch_size:(iters+1)*batch_size]
+        batch_t = t[iters*batch_size:(iters+1)*batch_size]
+
+        # 勾配を求め、パラメータを更新
+        loss = model.forward(batch_x, batch_t)
+        model.backward()
+        optimizer.update(model.params, model.grads)
+
+        total_loss += loss
+        loss_count += 1
+
+        # 定期的に学習経過を出力
+        if (iters+1) % 10 == 0:
+            avg_loss = total_loss / loss_count
+            print('| epoch %d |  iter %d / %d | loss %.2f'
+                  % (epoch + 1, iters + 1, max_iters, avg_loss))
+            loss_list.append(avg_loss)
+            total_loss, loss_count = 0, 0
+
+
+# 学習結果のプロット
+plt.plot(np.arange(len(loss_list)), loss_list, label='train')
+plt.xlabel('iterations (x10)')
+plt.ylabel('loss')
+plt.show()
+
+# 境界領域のプロット
+h = 0.001
+x_min, x_max = x[:, 0].min() - .1, x[:, 0].max() + .1
+y_min, y_max = x[:, 1].min() - .1, x[:, 1].max() + .1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+X = np.c_[xx.ravel(), yy.ravel()]
+score = model.predict(X)
+predict_cls = np.argmax(score, axis=1)
+Z = predict_cls.reshape(xx.shape)
+plt.contourf(xx, yy, Z)
+plt.axis('off')
+
+# データ点のプロット
+x, t = spiral.load_data()
+N = 100
+CLS_NUM = 3
+markers = ['o', 'x', '^']
+for i in range(CLS_NUM):
+    plt.scatter(x[i*N:(i+1)*N, 0], x[i*N:(i+1)*N, 1], s=40, marker=markers[i])
+plt.show()
+
